316db2dda837a0210726a94f28b7500902ca9a55
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2023-10-15 18:16:58 +0200

    Quick understanding of graphql Basics

--- Backend/schema.js
+++ Backend/schema.js
@@ -1,13 +1,48 @@
-const {makeSchema} = require("nexus")
+const {makeSchema, objectType, queryField, arg, mutationField, nullable, nonNull, intArg} = require("nexus")
 
-const Models = require("./resolvers/models");
-const Mutations = require("./resolvers/mutations");
-const Queries = require("./resolvers/queries");
+// const Models = require("./resolvers/models");
+// const Mutations = require("./resolvers/mutations");
+// const Queries = require("./resolvers/queries");
 
-const types = {...Models,...Mutations,...Queries}
+
 // console.log(types)
+
+// Modele
+const User = objectType({
+    name: 'User',
+    definition(t){
+        t.int('id');
+        t.string('Email')
+    }
+})
+
+// Query
+const usersGet = queryField('users',{
+    type: User,
+    resolve: async(root,args) => {
+        return {
+            id: 1,
+            Email :"oe"
+        }
+    }
+})
+
+// Mutation
+const userMutate = mutationField("createUser",{
+    type: nullable(User),
+    args:{
+        argid: nonNull(intArg())
+    },
+    resolve: async(root,args) => {
+        return {
+            id:2,
+            Email : "nan"
+        }
+    }
+})
+
 const schema = makeSchema({
-    types
+    types : [User,usersGet,userMutate]
 })
 
 module.exports = schema;
