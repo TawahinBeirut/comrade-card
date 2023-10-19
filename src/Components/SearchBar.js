653cfe528c101eaef96116c3134b6ae8d513f4b1
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2023-10-19 11:21:45 +0200

    Quasi Fin de la page Home, ajout du component SearchBar, plus que le responsive de la page

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ({text,onClick}) {
+
+  return (
+    <div className="h-14 shadow-sm rounded-xl bg-pink-600">
+        <input className="h-full rounded-lg w-96 text-center"type='text' placeholder={text}/>
+        <button className='w-14' onClick={onClick}><span class="material-symbols-outlined">travel_explore</span></button>
+    </div>
+  )
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,16 +3,21 @@ import { useParams } from 'react-router-dom'
 import NavBar from '../Components/NavBar'
 import StylisedLink from '../Components/StylisedLink'
 import Style from '../Utils/Style'
+import SearchBar from '../Components/SearchBar'
 
 export default function Home() {
+
+
+  
+  // On vérifie si il est authentifié
+  const userId = 2
 
   // On importe nos styles
   const LogoStyle = new Style("HomeLogo","ComradeCard","/");
   const ExploreStyle = new Style("HomeExplore","Explore","/Explore");
   const LoginStyle = new Style("HomeLogin","Login","/Login");
+  const ProfileStyle = new Style("HomeLogin","Profile",`/Profile/${userId}`)
 
-  
-  // On vérifie si il est authentifié
 
   return (
     <div className='bg-indigo-500'>
@@ -23,8 +28,17 @@ export default function Home() {
         Link={<StylisedLink Style={ExploreStyle}/>} 
         Link2={<StylisedLink Style={LoginStyle}/>}
         />
+        
+        <div className="flex justify-center align-middle w-full h-auto mt-80">
+          <div className="-ml-5">
+            <SearchBar text="Recherchez des produits ... "/>
+          </div>
+          </div>
+        
+        {/* Faire le Texte de Présentation du SITE */}
+        <div className=''>
 
-
+        </div>
       </div>
     </div>
   )
