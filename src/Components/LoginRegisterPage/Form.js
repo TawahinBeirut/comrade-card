19e32e8c26f35b7e2a9fed5bbb7e83c623d078c8
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2023-10-23 23:48:08 +0200

    Partie Style Front finie, plus que la logique à coder pour les pages Login et Register

--- src/Components/LoginRegisterPage/Form.js
+++ src/Components/LoginRegisterPage/Form.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Form({FormType,onClick,onChange}) {
+
+  const InputStyle = "h-10 rounded-xl border border-black ring-offset-2 ring-2 ring-red-800 text-center font-SearchBar focus:ring-2 ring-offset-transparent"
+  const toLink = (FormType.Nature === "Login") ? "/Register" : "/Login";
+  const TextLink = (FormType.Nature !== "Login") ? "Already registered? Log in" : "No account? Please register"
+
+  console.log(FormType)
   return (
-    <div className='border border-white m-9 w-4/5 h-4/5'>
+    <div className='m-9 w-4/5 h-4/5 flex flex-col text-center justify-around'>
+
+      <h1 className='text-white font-Logo text-5xl'>{FormType.Nature}</h1>
       {(FormType.Name !== undefined) ? 
-      <input type='text' value={FormType.Name} className=''></input> 
+      <input type='text' value={FormType.Name} className={InputStyle} placeholder='Name'></input>
       : null }
+      {(FormType.Email !== undefined) ? 
+      <input type='text' value={FormType.Email} className={InputStyle} placeholder='Email'></input>
+      : null}
+      {(FormType.Password !== undefined) ? 
+      <input type='text' value={FormType.Password} className={InputStyle}placeholder='Password'></input>
+      : null}
+      <button className=' rounded-3xl text-white h-10 bg-gradient-to-r from-red-600 via-purple-900 to-pink-300 font-SearchBar text-xl'>{FormType.Nature}</button>
+
+
+      <Link to={toLink} className='text-white decoration-4 decoration-white font-SearchBar' onClick={async() => {await new Promise(r => setTimeout(r,100)); window.location.reload()}}>{TextLink}</Link>
     </div>
   )
 }
--- src/Pages/LoginRegister.js
+++ src/Pages/LoginRegister.js
@@ -1,8 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import Cookies from 'js-cookie';
 import dataJson from '../data.json'
-import img from '../assets/Login_Background.png'
 import NavBar from '../Components/NavBar';
 import {Style} from '../Utils/Models';
 import StylisedLink from '../Components/StylisedLink';
@@ -10,21 +9,22 @@ import Form from '../Components/LoginRegisterPage/Form';
 
 export default function LoginRegister() {
 
-  // Premiere Etape : Supprimer les cookies de la session courante
+ // Premiere Etape : Supprimer les cookies de la session courante
   Cookies.remove(dataJson.Cookie_Name);
 
-  // Deuxieme Etape : Determiner si il s'agit d'un Login ou pas
-  const Nature = useLocation().pathname
-  const LoginCheck = (Nature === "/Login") ? true : false;
+ // Deuxieme Etape : Determiner si il s'agit d'un Login ou pas
+   const Nature = useLocation().pathname
+   const LoginCheck = (Nature === "/Login") ? true : false;
   
   // On definit nos formulaires en fonction
-  const LoginForm = {Email : "",Password : ""};
-  const RegisterForm = {...LoginForm,Name : ""}
-  const DefaultForm = (LoginCheck) ? LoginForm : RegisterForm
+  const LoginForm = {Nature : "Login", Email : "",Password : ""};
+  const RegisterForm = {...LoginForm,Name : "",Nature : "Register"}
 
+
+  const DefaultForm = (LoginCheck) ? LoginForm : RegisterForm
   const [FormType,setFormType] = useState(DefaultForm);
   
-  console.log(FormType)
+  
   // Un evenement OnChange pareil pour les 2 types de formulaire
   // Un evenement OnClick differents , l'un crée un compte et l'autre se connecte
   // Selon la Nature de la Page => Proposer un formulaire Different => Composant Form
@@ -37,7 +37,7 @@ export default function LoginRegister() {
     <div className="bg-LoginBack bg-cover h-screen">
       <div className="ml-44 flex justify-center"><NavBar Logo={<StylisedLink Style={LogoStyle}/>}/></div>
       <div className="flex justify-center align-middle">
-        <div className='border border-white mt-24 w-96 h-96'>
+        <div className=' mt-20 w-96 h-96'>
           <Form FormType={FormType} onClick="" onChange=""/>
         </div>
       </div>
